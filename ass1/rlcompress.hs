9dd8de0225b01471593820ae031fedbfe163e1b0
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-06-07 19:19:48 +0200

    Minor changes

--- ass1/rlcompress.hs
+++ ass1/rlcompress.hs
@@ -10,12 +10,12 @@ main = do
     let outputFile = args!!1
 
     -- read and compress the input text 
-    inputText <- readFile inputFile;
-    let outputText = rlcompress inputText
+    originalText <- readFile inputFile
+    let compressedText = rlcompress originalText
     
     -- get the length of the input and output
-    let orginalLength = length inputText
-    let compressedLength = length outputText
+    let orginalLength = length originalText
+    let compressedLength = length compressedText
     putStrLn("length of " ++ inputFile ++ ": " ++ (show orginalLength) ++ " characters")
     putStrLn("length of compressed file " ++ outputFile ++ ": " ++ (show compressedLength) ++ " characters")
 
@@ -24,20 +24,20 @@ main = do
     putStrLn("factor: " ++ (show compressedLength) ++ "/" ++ (show orginalLength) ++ "*100=" ++ (show compressionFactor) ++ "%")
 
     -- write the compressed string to the previously specified output file
-    writeFile outputFile outputText
+    writeFile outputFile compressedText
     putStrLn("done...")
 
 ----------------------------------------------
 
 -- compresses a string according to the run-length algorithm
 -- 1. group the string              ["aaa", "bb", "c", "ddd"]
--- 2. compresses each group         ["3a", "2b", "c", "3d"])
+-- 2. compresses each group         ["3a", "2b", "c", "3d"]
 -- 3. use concat to make one string ["3a2bc3d"]
 rlcompress :: String -> String
-rlcompress x = concat $ map rlcompressString $ group x
+rlcompress x = concat $ map rlcompressEntry $ group x
 
 -- compresses a string with the same characters
-rlcompressString :: String -> String
-rlcompressString x
+rlcompressEntry :: String -> String
+rlcompressEntry x
     | length x>1  = (show . length $ x) ++ [head x]
     | otherwise   = [head x]
